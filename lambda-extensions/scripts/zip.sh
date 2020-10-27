4c6ea945e013f3f8e9294b052b9131744370fa6a
sourabh <sourabh@sumologic.com>
2020-10-27 12:11:22 +0530

    updating zip.sh

--- lambda-extensions/scripts/zip.sh
+++ lambda-extensions/scripts/zip.sh
@@ -14,10 +14,9 @@ cd ..
 
 # Create lambda Layer from the new ZIP file in the provided AWS_PROFILE aws account.
 export AWS_PROFILE="personal"
-declare -a AWS_REGIONS=("us-east-1")
-# "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1s" "sa-east-1" "ap-east-1s" "af-south-1s" "eu-south-1" "me-south-1s")
+declare -a AWS_REGIONS=("us-east-1" "us-east-2" "us-west-1" "us-west-2" "ap-south-1" "ap-northeast-2" "ap-southeast-1" "ap-southeast-2" "ap-northeast-1" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "eu-north-1s" "sa-east-1" "ap-east-1s" "af-south-1s" "eu-south-1" "me-south-1s")
 
-# We have layer name as sumologic-extension.
+# We have layer name as sumologic-extension. Please change name for local testing.
 export layer_name="sumologic-extension"
 
 for region in "${AWS_REGIONS[@]}"
