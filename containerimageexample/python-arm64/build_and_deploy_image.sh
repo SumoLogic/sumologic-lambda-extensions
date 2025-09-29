055d96e6ea293f7b6ffd9334e38f215b61b2293f
Akhil Dangore <akhil.dangore.ctr@sumologic.com>
2025-09-29 17:04:56 +0530

    Updated Docker build

--- containerimageexample/python-arm64/build_and_deploy_image.sh
+++ containerimageexample/python-arm64/build_and_deploy_image.sh
@@ -1,7 +1,7 @@
-docker build -t lambda/hello-world-python:3.9-alpine3.12 .
+docker build --platform=linux/arm64 -t lambda/hello-world-python:3.13.7-slim-bookworm .
 
 ## Command to run container
-# docker run -p 9000:8080 lambda/hello-world-python:3.9-alpine3.12
+# docker run -p 9000:8080 lambda/hello-world-python:3.13.7-slim-bookworm
 
 ## Command to test
 # curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
@@ -9,6 +9,6 @@ docker build -t lambda/hello-world-python:3.9-alpine3.12 .
 ## Command to push image
 ACCOUNT_ID=956882708938
 aws ecr create-repository --repository-name hello-world-python-arm64 --image-scanning-configuration scanOnPush=true
-docker tag lambda/hello-world-python:3.9-alpine3.12 "${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/"hello-world-python-arm64:latest
+docker tag lambda/hello-world-python:3.13.7-slim-bookworm "${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/"hello-world-python-arm64:latest
 aws ecr get-login-password | docker login --username AWS --password-stdin "${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com"
 docker push "${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/hello-world-python-arm64:latest"
